8f9afc35918dbf929ddd185ea1605602972465e7
Thomas Houghton <tomhoughton888@gmail.com>
2022-08-13 17:02:39 +0100

    Implemented Hash

---
+++ NoobChain/src/BlockIDUtil.java
@@ -0,0 +1,25 @@
+import java.security.MessageDigest;
+
+public class BlockIDUtil {
+    // Simply applies the SHA256 hash to a string and returns.
+
+    public static String applySHA256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- NoobChain/src/Main.java
+++ NoobChain/src/Main.java
@@ -1,9 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String myHash = BlockIDUtil.applySHA256("my name is tome");
 
-
-
-
+        System.out.println("my hash");
+        System.out.println(myHash);
     }
 }
